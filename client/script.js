d2997d355375f5977b66ee54fb755ec4d9054bac
Praveen <foolmax569@gmail.com>
2026-05-14 20:16:47 +0400

    Fixed frontend connection

--- client/script.js
+++ client/script.js
@@ -1,22 +1,38 @@
 const form = document.getElementById("contactForm");
 
 form.addEventListener("submit", async (e) => {
+
     e.preventDefault();
 
     const data = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
+        message: document.getElementById("message").value
     };
 
-    const response = await fetch("https://portfolio-backend-d523.onrender.com", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    });
+    try {
 
-    const result = await response.json();
-    alert(result.message);
+        const response = await fetch("https://portfolio-backend-d523.onrender.com/contact", {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+
+        alert(result.message);
+
+    } catch (error) {
+
+        console.log(error);
+
+        alert("Error Connecting Backend");
+
+    }
+
 });
